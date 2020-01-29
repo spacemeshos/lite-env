81231b8d1f27f43bfdb9f5b9647eb2627207b099
ilans <buldov@gmail.com>
2020-01-29 13:25:39 +0200

    using poet ip

--- node-entrypoint.sh
+++ node-entrypoint.sh
@@ -3,14 +3,18 @@
 
 apk -q add --update wget curl jq iptables gettext
 
-export NODES_IP=$(getent hosts ${BS_NODE_URL} | awk '{ print $1 }')
 export EXT_PORT=7513
 export COINBASE="0x1234"
 
 if ! $BOOTSTRAP ; then
   echo "- BOOTSTRAP NODE -"
   echo "- GENESIS_ACTIVE_SIZE=$GENESIS_ACTIVE_SIZE -"
-  wget -qO- --tries=0 --retry-connrefused ${POET_URL}:8080/v1/info
+  if [ $(getent hosts ${BS_NODE_URL} | awk '{ print $1 }')!= $EXT_IP ]; then
+    echo "BS_NODE_URL doesn't resolve to ${EXT_IP}"
+    exit 1
+  fi
+  export POET_IP=$(getent hosts ${POET_URL} | awk '{ print $1 }')
+  wget -qO- --tries=0 --retry-connrefused ${POET_IP}:8080/v1/info
   export GENESIS_TIME=$(date -d "@$(($(date +%s) + $GENESIS_SEC_DELAY))" --utc +%Y-%m-%dT%H:%M:%S+00:00)
   echo "- GENESIS_TIME: $GENESIS_TIME -"
   envsubst < /root/config/config.toml.tmpl > ./config.toml
@@ -20,8 +24,7 @@ else
   iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 7513 -j REDIRECT --to-port ${EXT_PORT}
   iptables -t nat -A PREROUTING -i eth0 -p udp --dport 7513 -j REDIRECT --to-port ${EXT_PORT}
   iptables-save
-  until [ -f /root/config/config.toml ]; do sleep 1; done
-  cp  /root/config/config.toml ./config.toml
+  wget -qO- --tries=0 --retry-connrefused ${BS_NODE_URL} -O ./config.toml
 fi
 
 set -o xtrace
@@ -40,13 +43,13 @@ set +o xtrace
 bg_pid=$!
 
 until [ -d /root/spacemesh/nodes ]; do sleep 1; done
-export P2P="\"spacemesh://`ls /root/spacemesh/nodes`@${NODES_IP}:${EXT_PORT}\""
+export P2P="\"spacemesh://`ls /root/spacemesh/nodes`@${EXT_IP}:${EXT_PORT}\""
 echo "P2P: $P2P"
 
 if ! $BOOTSTRAP ; then
   export BOOTSTRAP=true
   envsubst < /root/config/config.toml.tmpl > /root/config/config.toml
-  wget -qO- --tries=0 --retry-connrefused --post-data '{ "gatewayAddresses": ["'${BS_NODE_URL}':9091"] }' ${POET_URL}:8080/v1/start
+  wget -qO- --tries=0 --retry-connrefused --post-data '{ "gatewayAddresses": ["'${EXT_IP}':9091"] }' ${POET_IP}:8080/v1/start
   echo "- POET STARTED -"
 fi
 
--- nodes-startup.sh
+++ nodes-startup.sh
@@ -1,6 +1,8 @@
 toolbox /google-cloud-sdk/bin/gsutil -m cp -r gs://spacemesh/sm/* .
 cd /var/lib/toolbox/*/
 export $(grep GENESIS_ACTIVE_SIZE config.env)
+export EXT_IP=$(curl -s -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+echo "EXT_IP: ${EXT_IP}"
 docker rm $(docker ps -a -q) ; docker volume prune -f ; docker network prune -f
 docker pull spacemeshos/go-spacemesh:develop
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v $PWD:$PWD -w $PWD docker/compose -f docker-compose-nodes.yml up -d --scale node=$GENESIS_ACTIVE_SIZE
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v $PWD:$PWD -w $PWD -e "EXT_IP=${EXT_IP}" docker/compose -f docker-compose-nodes.yml up -d --scale node=$GENESIS_ACTIVE_SIZE
