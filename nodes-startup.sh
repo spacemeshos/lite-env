92936bd3c3e96fe58dcd832e734bd888a295d104
Aviv Eyal <aviveyal07@gmail.com>
2020-03-10 13:27:22 +0200

    Use node v0.1.3 instead of develop from dockerhub

--- nodes-startup.sh
+++ nodes-startup.sh
@@ -4,5 +4,5 @@ export $(grep GENESIS_ACTIVE_SIZE config.env)
 export EXT_IP=$(curl -s -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
 echo "EXT_IP: ${EXT_IP}"
 docker rm $(docker ps -a -q) ; docker volume prune -f ; docker network prune -f
-docker pull spacemeshos/go-spacemesh:develop
+docker pull spacemeshos/go-spacemesh:v0.1.3
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v $PWD:$PWD -w $PWD -e "EXT_IP=${EXT_IP}" docker/compose -f docker-compose-nodes.yml up -d --scale node=$GENESIS_ACTIVE_SIZE
