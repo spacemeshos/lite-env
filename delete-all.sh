0c0c23a325631aa586d30131090a69a7fb440612
ilans <buldov@gmail.com>
2020-05-04 20:15:50 +0300

    env vars in delete-all

--- delete-all.sh
+++ delete-all.sh
@@ -5,21 +5,13 @@ out_delete () {
    echo "$(tput bold)$(tput setaf 1) DELETE: $@ $(tput sgr 0)"
 }
 
-if (( $(gcloud compute instances list --filter="name=(poet)" 2> /dev/null | wc -l | awk '{print $1}') > 0 )); then
-  out_delete "poet"
-  gcloud --quiet compute instances delete poet --zone us-east1-c &
-fi
+PROJECT=spacemesh-198810
+ZONE=us-east1-c
 
-if (( $(gcloud compute instances list --filter="name=(metrics)" 2> /dev/null | wc -l | awk '{print $1}') > 0 )); then
-  out_delete "metrics"
-  gcloud --quiet compute instances delete metrics --zone us-east1-c &
+VM=$(gcloud compute instances list --project $PROJECT --zones $ZONE --filter="name=(poet metrics nodes)" --format="value(name)" | xargs echo)
+out_delete "$VM"
+if [ -n "$VM" ]; then
+  gcloud --quiet compute instances delete --project $PROJECT --zone $ZONE $VM
 fi
-
-if (( $(gcloud compute instances list --filter="name=(nodes)" 2> /dev/null | wc -l | awk '{print $1}') > 0 )); then
-  out_delete "nodes"
-  gcloud --quiet compute instances delete nodes --zone us-east1-c &
-fi
-
-wait
 
 out_delete "Done!"
